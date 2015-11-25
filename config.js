cfac797b1961715771b8ba48f297795eaf87f9d7
NC <nalin.chhibber@gmail.com>
2015-11-25 20:34:43 +0530

    remove config from .gitignore

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+	  	consumer_key: 'fwcWbggpA2Skv8QvURpoiYIlW',
+		consumer_secret: 's6q9rKsC6qa91Xbh3KhZJXT06tH8xH7aYtP6BSuJozZgNdRZNk',
+		access_token: '3954929774-uXPUUteSRCFaOyaRXsJtV08dhY3bnQvK3TfzxhS',
+		access_token_secret: 'NOkBm0AjZgmLuWOwlqiXOWQNl2OVLjSLaaVCDAk2BzkXN'
+	}
+
+};
